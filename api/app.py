148180eb61a519b344a9f1679353a4b036a325aa
ttiramisu <106864282+ttiramisu@users.noreply.github.com>
2025-03-19 19:46:39 +0800

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,17 +1,6 @@
-from flask import Flask, render_template, send_file
-import matplotlib.pyplot as plt
-import io
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
-# Animals
-
-tiger = [
-    [1900, 1910, 1920, 1930, 1940, 1950, 1960,
-     1970, 1980, 1990, 2000, 2010, 2020],
-    [100000, 90000, 80000, 70000, 50000, 40000,
-     30000, 20000, 8000, 5000, 3500, 3200, 3900]
-]
 
 ###############################
 ############ MAIN #############
@@ -23,7 +12,7 @@ def index():
     return render_template('index.html')
 
 ###############################
-############ ANIMAL ############
+############ ANIMAL ###########
 ###############################
 
 
